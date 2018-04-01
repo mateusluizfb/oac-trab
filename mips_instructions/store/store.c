f715546d5306f7bb88ad4d7f7fe750f7010f8fe4
Mateus Luiz <mateuslfreitasb@gmail>
2018-04-01 12:58:21 -0300

    Fix store implementation

--- mips_instructions/store/store.c
+++ mips_instructions/store/store.c
@@ -3,29 +3,25 @@
 #include "../../memory.h"
 
 void sw(uint32_t address, int16_t kte, int32_t dado) {
-  store_value(1, dado, address, kte);
+  int32_t *word_pointer = (int32_t*) memory; // Cast to char and interate with bytes
+  int actual_address = kte + (address * 4); // valor to offset mais o endereço * 4, para andar no andereço em words
+  word_pointer += actual_address; // Bytes offset
+
+  *word_pointer = dado; // Insere o dado no endereço
 }
 
 void sh(uint32_t address, int16_t kte, int16_t dado) {
-  store_value(2, dado, address, kte);
+  int16_t *half_word_pointer = (int16_t*) memory; // Cast to char and interate with bytes
+  int actual_address = kte + (address * 2); // valor to offset mais o endereço * 4, para andar no andereço em words
+  half_word_pointer += actual_address; // Bytes offset
+
+  *half_word_pointer = dado; // Insere o dado no endereço
 }
 
 void sb(uint32_t address, int16_t kte, int8_t dado) {
-  store_value(3, dado, address, kte);
-}
-
-void store_value(int store_type, void *data, uint32_t address, int16_t kte) {
-  if (store_type == 1) {
-    int32_t data_retyped = (int32_t) data;
-  } else if (store_type == 2) {
-    int16_t data_retyped = (int16_t) data;
-  } else if (store_type == 2) {
-    int8_t data_retyped = (int8_t) data;
-  }
-
-  char *byte_pointer = (char*) memory; // Cast to char and interate with bytes
+  int8_t *byte_pointer = (int8_t*) memory; // Cast to char and interate with bytes
   int actual_address = kte + address; // valor to offset mais o endereço * 4, para andar no andereço em words
   byte_pointer += actual_address; // Bytes offset
 
-  *byte_pointer = data; // Insere o dado no endereço
+  *byte_pointer = dado; // Insere o dado no endereço
 }
