b5207540fc7e21c21f69b508387e13f9bb887490
Mateus Luiz <mateuslfreitasb@gmail>
2018-05-03 19:28:45 -0300

    Adicionando alinhamento de expoentes

--- trab-3/trab-3.asm
+++ trab-3/trab-3.asm
@@ -58,6 +58,40 @@
 		andi $v0, $a0, 0x007FFFFF
 		jr $ra
 
+	SUB_EXP:
+		andi $a0, $a0, 0x7F800000
+		andi $a1, $a1, 0x7F800000
+		srl $a0, $a0, 23
+		srl $a1, $a1, 23
+		sub $v0, $a0, $a1
+		jr  $ra
+
+	NORM_SINGLE: # norm_single(valor) / retorna mantissa normalizada e quantos bits foram shiftados positivamente ou negativamente
+		addi $sp, $sp, -8
+		sw $s1, 4($sp)
+		sw $s0, 0($sp)
+
+		add $s1, $zero, $zero # expoente = 0
+
+		NORM_SINGLE_CHECAR_BIT_23:
+			andi $t0, $s0, 0x01000000 # Checa de o bit '23' é '1'
+			beq $s0, $zero, NORM_SINGLE_CHECAR_BIT_24 # não tiver bit no bit 23, então checa o bit 24
+			j NORM_SINGLE_RETORNAR # se tiver '1' no bit 23 então mantissa já está normalizada
+
+		NORM_SINGLE_CHECAR_BIT_24:
+			andi $t0, $t0, 0x02000000 # Checa de o bit '24' é '1'
+			beq $t0, $zero, NORM_SINGLE_RETORNAR # se não tiver bit no 24, então retorna a mantissa
+			srl $s0, $s0, 1
+			addi $s1, $zero, 1 # expoente++
+
+		NORM_SINGLE_RETORNAR:
+			add $v0, $s0, $zero
+			add $v1, $s1, $zero
+
+			lw $s0, 0($sp)
+			lw $s1, 4($sp)
+			addi $sp, $sp, 8 # desempilhar
+
 
 	SOMAR: # Função de soma: somar(a, b) // $s0 = a, $s1 = b, $s2 = get_man(a), $s3 = get_man(b)
 		addi $sp, $sp, -16 # Alocar espaço na pilha
@@ -77,9 +111,40 @@
 		jal GET_MAN
 		add $s4, $zero, $v0 # guarda valor da mantissa de 'b' em $s4
 
-		ori $s3, $s3, 0x01000000 # Adiciona o bit '1' no bit 23 da mantissa de 'a'
-		ori $s4, $s4, 0x01000000 # Adiciona o bit '1' no bit 23 da mantissa de 'b'
+		ori $s3, $s3, 0x00800000 # Adiciona o bit '1' no bit 23 da mantissa de 'a'
+		ori $s4, $s4, 0x00800000 # Adiciona o bit '1' no bit 23 da mantissa de 'b'
 
-		add $t0, $s3, $s4 # soma as mantissas
+		# SUBTRAIR expoente de A por expoente de B
 
+		add $a0, $s0, $zero
+		add $a1, $s1, $zero
+		jal SUB_EXP # subtrai os expoentes
+		beq $v0, $zero, SOMAR_MANTISSA # se o retorno for 0, então vai pra soma
+
+		slt $t0, $v0, $zero # se retorno < 0 seta $t0 pra 1
+		beq $zero, $t0, ALINHAR_EXPOENTE_B # se o retorno é positivo, alinha a mantissa de b
+
+		ALINHAR_EXPOENTE_A:
+			add $t0, $v0, $zero,
+			ALINHAR_EXPOENTE_A_FOR:
+				beq $t0, $zero, ALINHAR_EXPOENTE_A_FOR_END
+				srl $s3, $s3, 1
+				addi $t0, $t0, 1
+			ALINHAR_EXPOENTE_A_FOR_END:
+				j SOMAR_MANTISSA
+
+		ALINHAR_EXPOENTE_B:
+			add $t0, $v0, $zero
+			ALINHAR_EXPOENTE_B_FOR:
+				beq $t0, $zero, ALINHAR_EXPOENTE_B_FOR_END
+				srl $s4, $s4, 1
+				addi $t0, $t0, -1
+			ALINHAR_EXPOENTE_B_FOR_END:
+				j SOMAR_MANTISSA
+
+		SOMAR_MANTISSA:
+			add $a0, $s3, $s4 # soma as mantissas
+			jal NORM_SINGLE
+
+		# desempilhar
 		j SAIR
